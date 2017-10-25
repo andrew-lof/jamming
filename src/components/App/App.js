1db2b470ecb850d9550dc67fe27cb641b25541da
Andrew Lof <Andrew.Lof@pointclickcare.com>
2017-10-25 13:28:57 -0400

    add ability to add and remove tracks from playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,30 @@ class App extends Component {
       playlistTracks:[{name:'Test'}, {artist:'Testers'},{album:'The Test'}]
     };
     //this.searchResults = this.searchResults.bind(this);
-
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+  addTrack(track){
+    for (let i = 0; i < this.state.playlistTracks.length; i++) {
+      let currTrack = this.state.playlistTracks[i];
+      if(currTrack.id !== track.id){
+      //add song
+        this.state.playlistTracks.push(track);
+        this.setState({playlistTracks: this.state.playlistTracks});
+        return;
+      }
+    }
+  }
+  removeTrack(track){
+    for (let i = 0; i < this.state.playlistTracks.length; i++) {
+      let currTrack = this.state.playlistTracks[i];
+      if(currTrack.id === track.id){
+      //remove song
+        this.state.playlistTracks.splice(i,1);
+        this.setState({playlistTracks: this.state.playlistTracks});
+        return;
+      }
+    }
   }
   render() {
     return (
@@ -24,8 +47,8 @@ class App extends Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} />
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
         </div>
       </div>
     </div>
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,15 +3,25 @@ import './Track.css';
 
 
 class Track extends React.Component{
-
-
+	constructor(props){
+		super(props);
+		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
+	}
 	renderAction(){
 		if(this.props.isRemoval){
-			return '-';
+			return <a className="Track-action" onClick={this.removeTrack}>-</a>;
 		} else {
-			return '+';
+			return <a className="Track-action" onClick={this.addTrack}>+</a>;
 		}
 	}
+	addTrack(){
+		this.props.track = this.props.onAdd;
+	}
+	removeTrack(){
+		this.props.track = this.props.onRemove;
+	}
+
 	render(){
 		return(
 			<div className="Track">
@@ -19,7 +29,7 @@ class Track extends React.Component{
 			    <h3>{this.props.track.name}</h3>
 			    <p>{this.props.track.artist} | {this.props.track.album}</p>
 			  </div>
-			  <a className="Track-action">{this.renderAction()}</a>
+			  {this.renderAction()}
 			</div>
 			);
 	}
